d0e98fb62070832aabd618341811925a103800c3
Naman Kumar <96226185+matcauthon49@users.noreply.github.com>
2022-02-18 08:32:56 +0530

    Add files via upload

---
+++ graph/random_tree_generator.py
@@ -0,0 +1,38 @@
+import random
+import pandas as pd
+
+def random_tree_generator(v):
+
+    adjacency_list = {
+        i:[] for i in range(1,v+3)
+    }
+    prufer = [
+        random.randint(1, v+1) for i in range(1, v+1)
+    ]
+    check_list = {
+        i for i in range(1, v+3)
+    }
+
+    for i in range(v):
+
+        v_1 = min(check_list.difference(prufer))
+        v_2 = prufer[i]
+
+        adjacency_list[v_1].append(v_2)
+        adjacency_list[v_2].append(v_1)
+
+        check_list.remove(v_1)
+        prufer[i] = -1
+
+    v_1 = check_list.pop()
+    v_2 = check_list.pop()
+
+    adjacency_list[v_1].append(v_2)
+    adjacency_list[v_2].append(v_1)
+
+    return(adjacency_list)
+
+generated_tree = random_tree_generator(random.randint(100, 1000))
+
+tree_series = pd.Series(generated_tree)
+tree_series.to_csv("tree.csv")
