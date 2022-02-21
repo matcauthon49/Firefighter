b2b0b34c5dae8487e0990b961c67dce06c7eaf2d
Naman Kumar <96226185+matcauthon49@users.noreply.github.com>
2022-02-21 22:57:02 +0530

    Create game_parameters_alternate.py

---
+++ graph/game_parameters_alternate.py
@@ -0,0 +1,54 @@
+import random
+
+class fgraph:
+
+    def __init__(self, adjacency_dict):
+        self.length = len(adjacency_dict)
+        self.adjacency_list = adjacency_dict
+        self.on_fire = {1}
+        self.currently_burning = {1}
+        self.protected = set()
+        self.playable = self.length - 1
+
+    def protect(self,i):
+        self.protected.add(i)
+        self.playable -= 1
+
+    def burn(self):
+        new_burning = set()
+        
+        while (self.currently_burning != set()):
+            burning_node = self.currently_burning.pop()
+
+            for i in self.adjacency_list[burning_node]:
+                if (i not in self.protected) and (i not in self.on_fire):
+                    new_burning.add(i)
+                    self.on_fire.add(i)
+                    self.playable -= 1
+
+        self.currently_burning = new_burning
+
+    def protect_random_vertex(self):
+        unprotected = ({i for i in range(1, self.length+1)} - self.on_fire)-self.protected
+
+        vertex_to_protect = random.choice(list(unprotected))
+        self.protect(vertex_to_protect)
+
+    def return_protected(self):
+        return self.protected
+
+    def return_on_fire(self):
+        return self.on_fire
+
+def play_firefighter(game_fgraph):
+
+    while (game_fgraph.playable != 0):
+        game_fgraph.protect_random_vertex()
+        game_fgraph.burn()
+
+    return (
+      game_fgraph.protected, 
+      game_fgraph.on_fire, 
+      len(game_fgraph.protected),
+      len(game_fgraph.on_fire)
+    )
